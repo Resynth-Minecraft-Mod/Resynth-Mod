c4ae38529c8bb63d21a08b777a16f10c6bf8e478
Luke Melaia <lukemelaia.personal@gmail.com>
2018-09-29 01:42:21 +0200

    Added ability to turn dirt/grass into mineral soil.

--- src/main/java/com/ki11erwolf/resynth/item/ItemMineralCrystal.java
+++ src/main/java/com/ki11erwolf/resynth/item/ItemMineralCrystal.java
@@ -15,6 +15,21 @@
  */
 package com.ki11erwolf.resynth.item;
 
+import com.ki11erwolf.resynth.block.ResynthBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
 /**
  * Mineral Crystal. Recipe component for Mineral Soil.
  */
@@ -26,5 +41,49 @@ public class ItemMineralCrystal extends ResynthItem {
      */
     public ItemMineralCrystal() {
         super("mineralCrystal");
+    }
+
+    /**
+     * {@inheritDoc}
+     * Turns grass/dirt into mineral soil
+     * at the cost of this item.
+     *
+     * @param player
+     * @param worldIn
+     * @param pos
+     * @param hand
+     * @param facing
+     * @param hitX
+     * @param hitY
+     * @param hitZ
+     * @return
+     */
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand,
+                                      EnumFacing facing, float hitX, float hitY, float hitZ){
+        Block type = worldIn.getBlockState(pos).getBlock();
+
+        if(type == Blocks.DIRT || type == Blocks.GRASS){
+            worldIn.setBlockState(pos, ResynthBlocks.BLOCK_MINERAL_SOIL.getDefaultState());
+            player.getHeldItem(hand).shrink(1);
+            return EnumActionResult.SUCCESS;
+        }
+
+        return EnumActionResult.FAIL;
+    }
+
+    /**
+     * {@inheritDoc}
+     * Adds a tooltip on how to use the item.
+     *
+     * @param stack
+     * @param worldIn
+     * @param tooltip
+     * @param flagIn
+     */
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip,
+                               ITooltipFlag flagIn){
+        tooltip.add("Right click on dirt or grass to turn it into mineral soil.");
     }
 }
