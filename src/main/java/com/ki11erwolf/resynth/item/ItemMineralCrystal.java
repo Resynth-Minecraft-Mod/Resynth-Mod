b0c16e68b43c34f475297fa6aacd78080fa8acd4
Luke Melaia <lukemelaia.personal@gmail.com>
2018-10-17 04:34:33 +0200

    Added some effects and sound to the mineral crystal.

--- src/main/java/com/ki11erwolf/resynth/item/ItemMineralCrystal.java
+++ src/main/java/com/ki11erwolf/resynth/item/ItemMineralCrystal.java
@@ -17,15 +17,18 @@ package com.ki11erwolf.resynth.item;
 
 import com.ki11erwolf.resynth.block.ResynthBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -64,6 +67,11 @@ public class ItemMineralCrystal extends ResynthItem {
         Block type = worldIn.getBlockState(pos).getBlock();
 
         if(type == Blocks.DIRT || type == Blocks.GRASS){
+
+            worldIn.playSound(null, pos, SoundEvents.BLOCK_GRASS_PLACE,
+                    SoundCategory.BLOCKS, 0.4F, 1.2F / (worldIn.rand.nextFloat() * 0.2F + 0.9F));
+            spawnBlockChangeParticles(worldIn, pos, 100);
+
             worldIn.setBlockState(pos, ResynthBlocks.BLOCK_MINERAL_SOIL.getDefaultState());
             player.getHeldItem(hand).shrink(1);
             return EnumActionResult.SUCCESS;
@@ -85,5 +93,38 @@ public class ItemMineralCrystal extends ResynthItem {
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip,
                                ITooltipFlag flagIn){
         tooltip.add("Right click on dirt or grass to turn it into mineral soil.");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void spawnBlockChangeParticles(World worldIn, BlockPos pos, int amount){
+        if (amount == 0){
+            amount = 15;
+        }
+
+        IBlockState iblockstate = worldIn.getBlockState(pos);
+
+        if (iblockstate.getMaterial() != Material.AIR) {
+            for (int i = 0; i < amount; ++i){
+                double d0 = itemRand.nextGaussian() * 0.02D;
+                double d1 = itemRand.nextGaussian() * 0.02D;
+                double d2 = itemRand.nextGaussian() * 0.02D;
+                worldIn.spawnParticle(EnumParticleTypes.SPELL,
+                        (double)((float)pos.getX() + itemRand.nextFloat()),
+                        (double)pos.getY() + (double)itemRand.nextFloat()
+                                * iblockstate.getBoundingBox(worldIn, pos).maxY,
+                        (double)((float)pos.getZ() + itemRand.nextFloat()), d0, d1, d2);
+            }
+        }
+        else {
+            for (int i1 = 0; i1 < amount; ++i1) {
+                double d0 = itemRand.nextGaussian() * 0.02D;
+                double d1 = itemRand.nextGaussian() * 0.02D;
+                double d2 = itemRand.nextGaussian() * 0.02D;
+                worldIn.spawnParticle(EnumParticleTypes.SPELL,
+                        (double)((float)pos.getX() + itemRand.nextFloat()),
+                        (double)pos.getY() + (double)itemRand.nextFloat() * 1.0f,
+                        (double)((float)pos.getZ() + itemRand.nextFloat()), d0, d1, d2);
+            }
+        }
     }
 }
