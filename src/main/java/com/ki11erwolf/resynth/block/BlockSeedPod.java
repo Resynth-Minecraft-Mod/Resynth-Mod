98655b7b390b0d7f0d8b67ee1799ecd86dbf1903
Luke Melaia <lukemelaia.personal@gmail.com>
2019-03-24 16:20:01 +0200

    Fixed errors in the Block Seed Pod class.

--- src/main/java/com/ki11erwolf/resynth/block/BlockSeedPod.java
+++ src/main/java/com/ki11erwolf/resynth/block/BlockSeedPod.java
@@ -19,7 +19,6 @@ import com.ki11erwolf.resynth.plant.PlantSetBiochemical;
 import com.ki11erwolf.resynth.plant.ResynthPlantSets;
 import com.ki11erwolf.resynth.util.MathUtil;
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
@@ -27,23 +26,22 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.common.IPlantable;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
-import static com.ki11erwolf.resynth.ResynthConfig.MYSTICAL_SEED_POD;
 
 /**
  * Mystical Seed Pod. A flower that spawns randomly in the
@@ -66,10 +64,10 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
     );
 
     /**
-     * Standard constructor.
+     * Default constructor.
      */
     public BlockSeedPod() {
-        super(Material.PLANTS, SoundType.PLANT, "seedPod");
+        super(Properties.create(Material.PLANTS), "seedPod");
     }
 
     /**
@@ -79,35 +77,11 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
      * @param pos
      * @return true if the block below is dirt or grass.
      */
-    @Override
+    //@Override
     public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
         IBlockState soil = worldIn.getBlockState(pos.down());
         return soil.getBlock().getClass() == Blocks.GRASS.getClass()
                 || soil.getBlock().getClass() == Blocks.DIRT.getClass();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param state
-     * @param source
-     * @param pos
-     * @return flower hit box
-     */
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return FLOWER_AABB;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param state
-     * @return false
-     */
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
     }
 
     /**
@@ -129,36 +103,8 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
      * @return plains plant type
      */
     @Override
-    public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
+    public EnumPlantType getPlantType(IBlockReader world, BlockPos pos) {
         return EnumPlantType.Plains;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param world
-     * @param pos
-     * @return this plants default state
-     */
-    @Override
-    public IBlockState getPlant(net.minecraft.world.IBlockAccess world, BlockPos pos) {
-        IBlockState state = world.getBlockState(pos);
-
-        if (state.getBlock() != this)
-            return getDefaultState();
-
-        return state;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @return cutout
-     */
-    @SideOnly(Side.CLIENT)
-    @Override
-    public BlockRenderLayer getBlockLayer() {
-        return BlockRenderLayer.CUTOUT;
     }
 
     /**
@@ -171,24 +117,10 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
      * @return undefined
      */
     @Override
-    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state,
+    @Nonnull
+    public BlockFaceShape getBlockFaceShape(IBlockReader worldIn, IBlockState state,
                                             BlockPos pos, EnumFacing face) {
         return BlockFaceShape.UNDEFINED;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param blockState
-     * @param worldIn
-     * @param pos
-     * @return none
-     */
-    @Nullable
-    @Override
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState,
-                                                 IBlockAccess worldIn, BlockPos pos) {
-        return NULL_AABB;
     }
 
     /**
@@ -197,32 +129,34 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
      * if seeds should be dropped.
      *
      * @param state
-     * @param rand
+     * @param worldIn
      * @param fortune
+     * @param pos
      * @return the seeds item to drop, the plant itself or nothing (config dependent).
      */
     @Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        if (MYSTICAL_SEED_POD.dropSeeds) {
+    @Nonnull
+    public IItemProvider getItemDropped(IBlockState state, World worldIn, BlockPos pos, int fortune) {
+        if (true/*TODO: config*/) {
             List<PlantSetBiochemical> plants = Arrays.asList(ResynthPlantSets.getBiochemicalPlantSets());
 
             PlantSetBiochemical plant = ResynthPlantSets.getBiochemicalPlantSets()[0];
 
-            for (int i = 0; i <= MYSTICAL_SEED_POD.triesPerBreak; i++) {
+            for (int i = 0; i <= 10/*TODO: config: MYSTICAL_SEED_POD.triesPerBreak*/; i++) {
                 plant = plants.get(MathUtil.getRandomIntegerInRange(0, plants.size() - 1));
                 if (MathUtil.chance(plant.getSeedPodDropPercentage())) {
                     return plant.getSeeds();
                 }
             }
 
-            if (MYSTICAL_SEED_POD.alwaysDropSeeds) {
+            if (true/*TODO: config: MYSTICAL_SEED_POD.alwaysDropSeeds*/) {
                 return plant.getSeeds();
             } else {
                 return Item.getItemFromBlock(Blocks.AIR);
             }
         }
 
-        return super.getItemDropped(state, rand, fortune);
+        return this;
     }
 
     /**
@@ -254,9 +188,10 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
      * @param flagIn
      */
     @Override
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip,
+    public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip,
                                ITooltipFlag flagIn) {
-        tooltip.add("Can drop seeds normally dropped by mobs for players in peaceful mode.");
+        tooltip.add(new TextComponentString(
+                "Can drop seeds normally dropped by mobs for players in peaceful mode."));
     }
 
     /**
@@ -269,8 +204,25 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
      */
     protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
         if (!this.canPlaceBlockAt(worldIn, pos)) {
-            this.dropBlockAsItem(worldIn, pos, state, 0);
+            //this.dropBlockAsItem(worldIn, pos, state, 0);
             worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
         }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param world
+     * @param pos
+     * @return this plants default state
+     */
+    @Override
+    public IBlockState getPlant(IBlockReader world, BlockPos pos) {
+        IBlockState state = world.getBlockState(pos);
+
+        if (state.getBlock() != this)
+            return getDefaultState();
+
+        return state;
     }
 }
