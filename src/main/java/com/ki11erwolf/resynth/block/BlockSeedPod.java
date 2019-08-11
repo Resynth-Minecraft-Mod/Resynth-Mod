b8aa6c569e8679c6b94096d41d0d436202b673f5
Luke Melaia <lukemelaia.personal@gmail.com>
2019-08-12 01:31:17 +0200

    Fixed up the functionality of the Mystical Seed Pod.

--- src/main/java/com/ki11erwolf/resynth/block/BlockSeedPod.java
+++ src/main/java/com/ki11erwolf/resynth/block/BlockSeedPod.java
@@ -15,61 +15,80 @@
  */
 package com.ki11erwolf.resynth.block;
 
+import com.ki11erwolf.resynth.config.ResynthConfig;
+import com.ki11erwolf.resynth.config.categories.SeedPodConfig;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.World;
-import net.minecraftforge.common.EnumPlantType;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReaderBase;
 import net.minecraftforge.common.IPlantable;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Arrays;
 import java.util.List;
 
-
+/**
+ * Mystic Seed Pod. Drops Biochemical seeds for players
+ * in peaceful mode.
+ */
 @SuppressWarnings("deprecation")
 public class BlockSeedPod extends ResynthBlock implements IPlantable {
 
-    protected static final VoxelShape FLOWER_AABB = Block.makeCuboidShape(
+    /**
+     * The configuration settings for this block.
+     */
+    @SuppressWarnings("unused")
+    private static final SeedPodConfig CONFIG = ResynthConfig.GENERAL_CONFIG.getCategory(SeedPodConfig.class);
+
+    /**
+     * The bounding/hit box (shape) for this block.
+     */
+    private static final VoxelShape SEED_POD_SHAPE = Block.makeCuboidShape(
              3.0D, 0.0D, 3.0D,
             14.0D, 15.0D, 14.0D
     );
 
-    public BlockSeedPod() {
-        super(Properties.create(Material.PLANTS), "seed_pod");
+    /**
+     * Default constructor.
+     */
+    BlockSeedPod() {
+        super(Properties.create(Material.PLANTS).sound(SoundType.PLANT), "seed_pod");
     }
 
-    //@Override
-    public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
-        IBlockState soil = worldIn.getBlockState(pos.down());
-        return soil.getBlock().getClass() == Blocks.GRASS.getClass()
-                || soil.getBlock().getClass() == Blocks.DIRT.getClass();
-    }
+    // **************
+    // Visual & Shape
+    // **************
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code false}.
+     */
     @Override
     public boolean isFullCube(IBlockState state) {
         return false;
     }
 
-    @Override
-    public EnumPlantType getPlantType(IBlockReader world, BlockPos pos) {
-        return EnumPlantType.Plains;
-    }
-
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@link BlockFaceShape#UNDEFINED}.
+     */
     @Override
     @Nonnull
     public BlockFaceShape getBlockFaceShape(IBlockReader worldIn, IBlockState state,
@@ -77,18 +96,166 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
         return BlockFaceShape.UNDEFINED;
     }
 
-    //TODO: Redo this.
-//    /**
-//     * {@inheritDoc}
-//     * Determines what seeds to drop (random percentage chance)
-//     * if seeds should be dropped.
-//     *
-//     * @param state
-//     * @param worldIn
-//     * @param fortune
-//     * @param pos
-//     * @return the seeds item to drop, the plant itself or nothing (config dependent).
-//     */
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@link Block.EnumOffsetType#XZ}.
+     */
+    @Override
+    public Block.EnumOffsetType getOffsetType() {
+        return Block.EnumOffsetType.XZ;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code false}.
+     */
+    @Override
+    public boolean isSolid(IBlockState state) {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@link BlockRenderLayer#CUTOUT}.
+     */
+    @Override
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    /**
+     * {@inheritDoc}.
+     *
+     * This does not affect the collision box of the plant as it's
+     * set by {@link #getCollisionShape(IBlockState, IBlockReader, BlockPos)}.
+     *
+     * @return the bounding box/hit box of this plant. {@link #SEED_POD_SHAPE}
+     * with the offset.
+     */
+    @Override
+    public VoxelShape getShape(IBlockState state, IBlockReader worldIn, BlockPos pos) {
+        Vec3d vec3d = state.getOffset(worldIn, pos);
+        return SEED_POD_SHAPE.withOffset(vec3d.x, vec3d.y, vec3d.z);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code 0}.
+     */
+    @Override
+    public int getOpacity(IBlockState state, IBlockReader worldIn, BlockPos pos) {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@link VoxelShapes#empty()}.
+     */
+    @Override
+    public VoxelShape getCollisionShape(IBlockState state, IBlockReader worldIn, BlockPos pos) {
+        return VoxelShapes.empty();
+    }
+
+    // ***************
+    // Plant Interface
+    // ***************
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return the default state of this block or it's
+     * state in the world at the given position.
+     */
+    @Override
+    public IBlockState getPlant(IBlockReader world, BlockPos pos) {
+        IBlockState state = world.getBlockState(pos);
+
+        if (state.getBlock() != this)
+            return getDefaultState();
+
+        return state;
+    }
+
+    // *******
+    // Tooltip
+    // *******
+
+    /**
+     * {@inheritDoc}.
+     *
+     * Sets the tooltip on what this block does/how to use it.
+     */
+    @Override
+    public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip,
+                               ITooltipFlag flagIn) {
+        tooltip.add(new TextComponentString(
+                "Can drop seeds normally dropped by mobs for players in peaceful mode."));
+    }
+
+    // *********
+    // Placement
+    // *********
+
+    /**
+     * Used to check if the given block is valid ground
+     * (i.e. if this plant block can be placed on it).
+     *
+     * @param state the block to check if it's valid ground.
+     * @return {@code true} if the given block is valid ground
+     * (this plant can be placed on it).
+     */
+    private boolean isValidGround(IBlockState state) {
+        Block block = state.getBlock();
+        return  block == Blocks.GRASS_BLOCK
+                || block == Blocks.DIRT
+                || block == Blocks.COARSE_DIRT
+                || block == Blocks.PODZOL;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code true} if the block underneath this block is valid ground
+     * ({@link #isValidGround(IBlockState)}).
+     */
+    @Override
+    public boolean isValidPosition(IBlockState state, IWorldReaderBase worldIn, BlockPos pos) {
+        BlockPos blockpos = pos.down();
+        //Forge: This function is called during world gen and placement,
+        //before this block is set, so if we are not 'here' then assume it's the pre-check.
+        if (state.getBlock() == this)
+            return worldIn.getBlockState(blockpos).canSustainPlant(worldIn, blockpos, EnumFacing.UP, this);
+
+        return this.isValidGround(worldIn.getBlockState(blockpos));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Checks if this plant block is still on {@link #isValidGround(IBlockState)}
+     * after a neighbour block changes.
+     *
+     * @return the updated state of this block. Can be air if the plant
+     * is no longer on valid ground.
+     */
+    @Override
+    public IBlockState updatePostPlacement(IBlockState stateIn, EnumFacing facing, IBlockState facingState,
+                                           IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        return !stateIn.isValidPosition(worldIn, currentPos)
+                ? Blocks.AIR.getDefaultState()
+                : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
+
+    // ***********
+    // Plant Logic
+    // ***********
+
+    //TODO: Fix.
 //    @Override
 //    @Nonnull
 //    public IItemProvider getItemDropped(IBlockState state, World worldIn, BlockPos pos, int fortune) {
@@ -113,61 +280,4 @@ public class BlockSeedPod extends ResynthBlock implements IPlantable {
 //
 //        return this;
 //    }
-
-    @Override
-    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos,
-                                Block blockIn, BlockPos fromPos) {
-        super.neighborChanged(state, worldIn, pos, blockIn, fromPos);
-        //this.checkAndDropBlock(worldIn, pos, state);
-    }
-
-    @Override
-    public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip,
-                               ITooltipFlag flagIn) {
-        tooltip.add(new TextComponentString(
-                "Can drop seeds normally dropped by mobs for players in peaceful mode."));
-    }
-
-    protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
-        if (!this.canPlaceBlockAt(worldIn, pos)) {
-            //this.dropBlockAsItem(worldIn, pos, state, 0);
-            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
-        }
-    }
-
-    @Override
-    public IBlockState getPlant(IBlockReader world, BlockPos pos) {
-        IBlockState state = world.getBlockState(pos);
-
-        if (state.getBlock() != this)
-            return getDefaultState();
-
-        return state;
-    }
-
-    @Override
-    public Block.EnumOffsetType getOffsetType() {
-        return Block.EnumOffsetType.XZ;
-    }
-
-    @Override
-    public boolean isSolid(IBlockState state) {
-        return false;
-    }
-
-    @Override
-    public BlockRenderLayer getRenderLayer() {
-        return BlockRenderLayer.CUTOUT;
-    }
-
-    @Override
-    public VoxelShape getShape(IBlockState state, IBlockReader worldIn, BlockPos pos) {
-        Vec3d vec3d = state.getOffset(worldIn, pos);
-        return FLOWER_AABB.withOffset(vec3d.x, vec3d.y, vec3d.z);
-    }
-
-    @Override
-    public int getOpacity(IBlockState state, IBlockReader worldIn, BlockPos pos) {
-        return 0;
-    }
 }
---
+++ src/main/java/com/ki11erwolf/resynth/config/categories/SeedPodConfig.java
@@ -0,0 +1,90 @@
+package com.ki11erwolf.resynth.config.categories;
+
+import com.ki11erwolf.resynth.config.ConfigBooleanValue;
+import com.ki11erwolf.resynth.config.ConfigCategory;
+import com.ki11erwolf.resynth.config.ConfigIntegerValue;
+
+/**
+ * Config settings for the {@link com.ki11erwolf.resynth.block.BlockSeedPod}.
+ */
+public class SeedPodConfig extends ConfigCategory {
+
+    // ************************
+    // Config Value Definitions
+    // ************************
+
+    /**
+     * Config value definition that allows enabling/disabling
+     * drops from the Mystical Seed Pod.
+     */
+    private final ConfigBooleanValue enabled = new ConfigBooleanValue(
+            "enable-drops-from-seed-pod",
+            "Set to true to make the Mystical Seed Pod drop a random Biochemical (mob drop)" +
+                      "\nseed when broken. This can be useful for players playing on peaceful mode who can" +
+                      "\notherwise not get mob drops",
+            false,
+            this
+    );
+
+    /**
+     * Config value definition that allows enabling/disabling
+     * the "always drop seeds" functionality of the Seed Pod.
+     */
+    private final ConfigBooleanValue alwaysDropSeeds = new ConfigBooleanValue(
+            "always-drop-seeds",
+            "Set to true to make sure the Mystical Seed Pod always drops seeds." +
+                      "\nThis does NOT override [enable-drops-from-seed-pod], it only makes" +
+                      "\nsure the plant will always drop seeds instead of occasionally dropping nothing.",
+            false,
+            this
+    );
+
+    /**
+     * Config value definition that allows setting the
+     * number of tries before giving up.
+     */
+    private final ConfigIntegerValue tries = new ConfigIntegerValue(
+            "number-of-tries",
+            "The amount of times the Mystical Seed Pod will try find a Biochemical" +
+                      "\nseed before giving up and dropping nothing. Disable this with [always-drop-seeds].",
+            10,
+            5, 150,
+            this
+    );
+
+    /**
+     * Creates a new, unique configuration category
+     * grouping.
+     */
+    public SeedPodConfig() {
+        super("mystical-seed-pod");
+    }
+
+    // ****************
+    // Public Accessors
+    // ****************
+
+    /**
+     * @return {@code true} if the config has enabled
+     * drops from the SeedPod.
+     */
+    public boolean areDropsEnabled(){
+        return enabled.getValue();
+    }
+
+    /**
+     * @return {@code true} if the config has
+     * enabled the "always drop seeds" functionality.
+     */
+    public boolean alwaysDropSeeds(){
+        return this.alwaysDropSeeds.getValue();
+    }
+
+    /**
+     * @return the amount of times to try and
+     * find valid seeds before giving up.
+     */
+    public int getTries(){
+        return this.tries.getValue();
+    }
+}
