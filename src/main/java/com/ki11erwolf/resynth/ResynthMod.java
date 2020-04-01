2ed1c9d8218672957b44b2432cda0b7ce9dd1daf
Luke Melaia <lukemelaia.personal@gmail.com>
2020-04-01 02:25:43 +0200

    Final commit for version 4.1.0

--- src/main/java/com/ki11erwolf/resynth/ResynthMod.java
+++ src/main/java/com/ki11erwolf/resynth/ResynthMod.java
@@ -56,7 +56,7 @@ public class ResynthMod {
     /**
      * Resynth version.
      */
-    public static final String MOD_VERSION = "4.2.0";
+    public static final String MOD_VERSION = "4.1.0";
 
     /**
      * Minecraft version.
